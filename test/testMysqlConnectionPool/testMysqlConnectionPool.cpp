bac4ae2b7a2e6c8d2a42a4d1fb51e17336a11535
Yap Miracle <2049829115@qq.com>
2024-02-07 00:15:42 +0800

    :fire: Mysql Pool 测试编写成功

--- test/testMysqlConnectionPool/testMysqlConnectionPool.cpp
+++ test/testMysqlConnectionPool/testMysqlConnectionPool.cpp
@@ -3,29 +3,37 @@
 
 using namespace std;
 
-int main(){
-    MySQLConnectionPool* mysqlPool = MySQLConnectionPool::getInstance();
+int main()
+{
+    MySQLConnectionPool *mysqlPool = MySQLConnectionPool::getInstance();
+    MySQLConnectionPool *mysqlPool1 = MySQLConnectionPool::getInstance();
+    if (mysqlPool == mysqlPool1)
+        cout << "instance is !!!!!" << endl;
     mysqlPool->initPool("tcp://127.0.0.1:3306", "chat", "root", "123456");
 
-    sql::Connection* conn = mysqlPool->getConnection();
-    if(nullptr == conn){
-        cout<<"get False"<<endl;
-    }else{
-        // sql::Statement *state;
-        // sql::ResultSet *res;
-        // string sql = "select * from User";
-        // state = conn->createStatement();
-        // res = state->executeQuery(sql);
-        // while(res->next()){
-        //     cout << "id = " << res->getInt(1) << endl;
-        //     cout << "name = " << res->getString(2) << endl;
-        //     cout << "password = " << res->getString(3) << endl;
-        //     cout << "state = " << res->getString(4) << endl;
-        // }
-        // delete res;
-        // delete state;
-        // mysqlPool->releaseConnection(conn);
-        cout << "Get instance !!!!" <<endl;
+    sql::Connection *conn = mysqlPool->getConnection();
+    if (nullptr == conn)
+    {
+        cout << "get False" << endl;
     }
-    return 0;    
+    else
+    {
+        sql::Statement *state;
+        sql::ResultSet *res;
+        string sql = "select * from User";
+        state = conn->createStatement();
+        res = state->executeQuery(sql);
+        while (res->next())
+        {
+            cout << "id = " << res->getInt(1) << endl;
+            cout << "name = " << res->getString(2) << endl;
+            cout << "password = " << res->getString(3) << endl;
+            cout << "state = " << res->getString(4) << endl;
+        }
+        delete res;
+        delete state;
+        mysqlPool->releaseConnection(conn);
+        // cout << "Get instance !!!!" <<endl;
+    }
+    return 0;
 }
